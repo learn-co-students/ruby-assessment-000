dee6e0ba5d0832c3a0a09cbe788988897ef0f333
gjc2118 <gjc2118@gmail.com>
2015-05-30 19:52:46 -0700

    Did I pass?

--- spec/level_1_spec.rb
+++ spec/level_1_spec.rb
@@ -2,7 +2,7 @@ describe "Level 1" do
 	describe "Arrays" do
 		it "adds an element to an array" do 
 			array = ["Blake","Steven","Jeff"]
-  		# add an element to the array
+  		array << "Jeff"
 			expect(array.count).to eq(4)
 		end
 
@@ -11,19 +11,19 @@ describe "Level 1" do
 			expect(STDOUT).to receive(:puts).with("Blake")
 			expect(STDOUT).to receive(:puts).with("Steven")
 			expect(STDOUT).to receive(:puts).with("Jeff")
-
+			array.each{|e| puts e}
 			# iterate over each element in the array and puts it
 		end
 
 		it "returns the value at index 0" do
 			array = ["Blake","Steven","Jeff"]
-			element_1 = :banana # replace banana with the code that finds the first element
+			element_1 = array[0] # replace banana with the code that finds the first element
 			expect(element_1).to eq("Blake") 
 		end
 
 		it "returns the user at an index" do
 			array = ["Blake","Steven","Jeff"]
-			jeff_index = :banana # replace banana with the code that finds Jeff's index
+			jeff_index = array.index("Jeff") # replace banana with the code that finds Jeff's index
 			expect(jeff_index).to eq(2)
 		end
 	end
--- spec/level_2_spec.rb
+++ spec/level_2_spec.rb
@@ -2,6 +2,7 @@ describe "Level 2" do
   describe "Hashes" do
     it "adds a new key" do
       instructor = {name: "Steven", age: 30}
+      instructor[:location] = "NYC"
       # Add a key :location with a value of "NYC" to instructor
       expect(instructor[:location]).to eq("NYC")
     end
@@ -10,19 +11,22 @@ describe "Level 2" do
       instructor = {name: "Steven", age: 30}
       expect(STDOUT).to receive(:puts).with("Key is name. Value is Steven")
       expect(STDOUT).to receive(:puts).with("Key is age. Value is 30")
+      instructor.each {|key,value| puts "Key is #{key}. Value is #{value}"}
       # Iterate over the hash and print each key value pair. 
       # Print using string interpolation to match the expectations.
     end
 
     it "returns a value from the hash" do
       instructor = {name: "Steven", age: 30}
-      name = :banana # extract name value from the hash
+      name = instructor[:name] # extract name value from the hash
       expect(name).to eq("Steven")
     end
 
     it "finds a key from a known value" do
        instructor = {name: "Steven", age: 30}
-       key_from_value = :banana # write code that finds a key where the value is 30
+       key_from_value = ""
+       instructor.each {|key, value| if value == 30 then key_from_value = key end
+       }# write code that finds a key where the value is 30
        expect(key_from_value).to eq(:age)
     end
   end
